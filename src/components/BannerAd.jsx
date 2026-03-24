15856d8a1e94e350bed81a05457a7b577f4dcf2b
nolansharry <sharryn.wit.edu>
2026-03-24 17:10:20 -0400

    banner ad done

--- src/components/BannerAd.jsx
+++ src/components/BannerAd.jsx
@@ -11,7 +11,7 @@ export default function BannerAd() {
 
   const showBanner = async () => {
     await AdMob.showBanner({
-      adId: 'ca-app-pub-xxxxxxxx/xxxxxxxx', // replace with your AdMob ad unit ID
+      adId: 'ca-app-pub-9582762087729330~7184286874', // replace with your AdMob ad unit ID
       adSize: BannerAdSize.BANNER,
       position: BannerAdPosition.BOTTOM_CENTER,
       margin: 72, 
